d405d8f9e8c5764c00bcb503325030d6db880942
Daniel Shteremberg <dshteremberg@smu.edu>
2015-04-07 23:49:44 -0500

    first commit
    project set up nothing of substance

---
+++ photoEXIFData/photoEXIFData/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  photoEXIFData
+//
+//  Created by Daniel Shteremberg on 4/7/15.
+//  Copyright (c) 2015 Daniel Shteremberg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
